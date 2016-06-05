c89efab35e40140a4590545027613f03347dc235
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-05 11:16:41 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -1925,9 +1925,9 @@ sub_4F5A:					; "^G" - bell?
 
 ; ----------------------------------------------------------------------------
 L4F6A:	.byte	$00
-L4F6B:	.byte	$00
-L4F6C:	.byte	$2E
+L4F6B:	.byte	$00,$2E
 
+; ----------------------------------------------------------------------------
 sub_4F6D:  
 	prolog
 	sta     L4F6A                           ; 4F70 8D 6A 4F                 .jO
@@ -1938,12 +1938,12 @@ sub_4F6D:
 	sta     L4F6B                           ; 4F7C 8D 6B 4F                 .kO
 	lda     #$00                            ; 4F7F A9 00                    ..
 	adc     $A1                             ; 4F81 65 A1                    e.
-	sta     L4F6C                           ; 4F83 8D 6C 4F                 .lO
+	sta     L4F6B+1
 L4F86:  jsr     sub_4BB8
 	lda     $A0                             ; 4F89 A5 A0                    ..
 	cmp     L4F6B                           ; 4F8B CD 6B 4F                 .kO
 	lda     $A1                             ; 4F8E A5 A1                    ..
-	sbc     L4F6C                           ; 4F90 ED 6C 4F                 .lO
+	sbc     L4F6B+1
 	lbcs	L4F9B
 L4F98:  jmp     L4F86                           ; 4F98 4C 86 4F                 L.O
 
