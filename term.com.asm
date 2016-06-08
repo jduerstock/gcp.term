b26899914929d336e7fd670f9c9951623f79016d
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-08 09:39:58 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -9242,30 +9242,7 @@ LAC70:  ldy     $A2                             ; AC70 A4 A2                    
 	ldi	$A0, $01
 	rts                                     ; AC7D 60                       `
 
-; ----------------------------------------------------------------------------
-
-sub_AC7E:
-	txa                                     ; AC7E 8A                       .
-	pha                                     ; AC7F 48                       H
-	lda     KBCODE
-	eor     CH1
-	bne     LAC8D                           ; AC86 D0 05                    ..
-	lda     KEYDEL
-	bne     LACA9                           ; AC8B D0 1C                    ..
-LAC8D:  lda     $D209                           ; AC8D AD 09 D2                 ...
-	sta     CH1
-	ldx     L474D                           ; AC93 AE 4D 47                 .MG
-	sta     $B138,x                         ; AC96 9D 38 B1                 .8.
-	inc     L474D                           ; AC99 EE 4D 47                 .MG
-	and8i	L474D, L474D, $0F
-	ldx     #$01                            ; ACA4 A2 01                    ..
-	stx     KEYDEL
-LACA9:	ldi	SRTIMR, $11
-	pla                                     ; ACAE 68                       h
-	tax                                     ; ACAF AA                       .
-	pla                                     ; ACB0 68                       h
-	rti                                     ; ACB1 40                       @
-
+	.include "sub-ac7e.asm"
 	.include "sub-acb2.asm"
 	.include "sub-ad07.asm"
 	.include "sub-ad2e.asm"
