c8d7f040a5f1a7bdb6f73514c98154dcc1c38990
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-09 08:38:47 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -8729,24 +8729,7 @@ LA7B7:  lda     LA6D9                           ; A7B7 AD D9 A6                 
 	jsr     cmd_d1
 	rts                                     ; A7C0 60                       `
 
-; ----------------------------------------------------------------------------
-LA7C1:  .byte	$00
-LA7C2:  .byte	$00
-LA7C3:  .byte	$00
-
-cmd_d7:
-	stack_prolog LA7C1, $02
-	jsr     cmd_d0
-	ldi	$A3, $00
-	ldi	$A4, $00
-	rdmv	$A5, LA7C2
-	ldy     #$20                            ; A7E2 A0 20                    . 
-	ldx     #$00                            ; A7E4 A2 00                    ..
-	lda     LA7C1                           ; A7E6 AD C1 A7                 ...
-	jsr     sub_45D0
-	jsr     cmd_d1
-	rts                                     ; A7EF 60                       `
-
+	.include "cmd-d7.asm"
 	.include "cmd-d8.asm"
 	.include "cmd-lu.asm"
 	.include "cmd-lv.asm"
