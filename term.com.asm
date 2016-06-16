dca3c118fbd73e6da093d57b866eed52db918ae5
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-16 09:25:33 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -5651,24 +5651,7 @@ L7FE2:  lda     L7F92                           ; 7FE2 AD 92 7F                 
 	sta     $A0                             ; 7FE5 85 A0                    ..
 	rts                                     ; 7FE7 60                       `
 
-; ----------------------------------------------------------------------------
-L7FE8:  .byte	$00
-
-; ----------------------------------------------------------------------------
-cmd_ul:
-	prolog
-	sta     L7FE8                           ; 7FEC 8D E8 7F                 ...
-	lda     L7FE8                           ; 7FEF AD E8 7F                 ...
-	lsr     a                               ; 7FF2 4A                       J
-	lsr     a                               ; 7FF3 4A                       J
-	lsr     a                               ; 7FF4 4A                       J
-	lsr     a                               ; 7FF5 4A                       J
-	sta     $A0                             ; 7FF6 85 A0                    ..
-	ldx     L7FE8                           ; 7FF8 AE E8 7F                 ...
-	lda     $A0                             ; 7FFB A5 A0                    ..
-	jsr     sub_49A2
-	rts                                     ; 8000 60                       `
-
+	.include "cmd-ul.asm"
 	.include "cmd-up.asm"
 	.include "sub-8020.asm"
 	.include "sub-8047.asm"
