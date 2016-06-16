b7d1dfa59781142873f830ebf6b4b9e63a221c91
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-16 19:39:46 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -3530,28 +3530,7 @@ L6328:	shladdi	off_AE, L466F, $0001
 	jsr     sub_6203
 	rts                                     ; 6368 60                       `
 
-; ----------------------------------------------------------------------------
-L6369:  .byte	$00
-L636A:	.byte	$C4                             ; 636A C4                       .
-L636B:	.byte	$02                             ; 636B 02                       .
-L636C:  .byte	$00
-L636D:  .byte	$00
-
-; ----------------------------------------------------------------------------
-sub_636E:
-	prolog
-	mv	L6369, L46EF
-	shladdm8 off_AE, L46F5, L6369 
-	ldp16	L636C
-	add16i	$A2, L636C, $0003
-	rdldi	$A4, $0005
-	ldy     $A2                             ; 63AF A4 A2                    ..
-	ldxa	L636A
-	jsr     blockmove
-	add16i	off_AE, L636C, $0002
-	ldp8	CHBAS
-	rts                                     ; 63D0 60                       `
-
+	.include "sub-636e.asm"
 	.include "sub-63dd.asm"
 	.include "sub-65b0.asm"
 	.include "cmd-uw.asm"
