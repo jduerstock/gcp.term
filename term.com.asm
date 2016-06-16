d697fad787001e014853b490683b4f8309002206
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-16 09:23:39 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -5681,24 +5681,7 @@ cmd_up:
 	jsr     sub_49D3
 	rts                                     ; 801F 60                       `
 
-; ----------------------------------------------------------------------------
-sub_8020:  
-	prolog
-	lda     #$00                            ; 8023 A9 00                    ..
-	sta     $A3                             ; 8025 85 A3                    ..
-	ldy     #$04                            ; 8027 A0 04                    ..
-	ldxai	L474F
-	jsr     bzero
-	lda     #$00                            ; 8030 A9 00                    ..
-	sta     $A3                             ; 8032 85 A3                    ..
-	ldy     #$0E                            ; 8034 A0 0E                    ..
-	ldxai	L4753
-	jsr     bzero
-	ldx     #$00                            ; 803D A2 00                    ..
-	lda     #$00                            ; 803F A9 00                    ..
-	jsr     cmd_up
-	rts                                     ; 8044 60                       `
-
+	.include "sub-8020.asm"
 	.include "sub-8047.asm"
 	.include "cmd-ur.asm"
 	.include "cmd-um.asm"
