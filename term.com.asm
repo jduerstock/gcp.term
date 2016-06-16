7ab95ccb44da137d5e03cab8acfd5228f23b8976
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-16 19:16:23 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -3960,18 +3960,7 @@ L68F4:	add8m	off_AE, L67D1, L67C6
 L6964:	yldi	L4656, $01
 	rts                                     ; 6969 60                       `
 
-; ----------------------------------------------------------------------------
-sub_696A:  
-	prolog
-	lda     #$1E                            ; 696D A9 1E                    ..
-	asl     a                               ; 696F 0A                       .
-	sta     $A2                             ; 6970 85 A2                    ..
-	ldi	$A3, $00
-	ldy     $A2                             ; 6976 A4 A2                    ..
-	ldxa	L46E2
-	jsr     bzero
-	rts                                     ; 6981 60                       `
-
+	.include "sub-696a.asm"
 	.include "cmd-uk.asm"
 	.include "cmd-uc.asm"
 	.include "cmd-ub.asm"
