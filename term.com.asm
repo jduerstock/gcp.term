b05899e0b585239990a9dbc626670773e6aa3aae
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-18 09:25:58 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -1587,17 +1587,7 @@ L4BA2:  iny                                     ; 4BA2 C8                       
 	sta     $A0                             ; 4BA4 85 A0                    ..
 	rts                                     ; 4BA6 60                       `
 
-; ----------------------------------------------------------------------------
-cmd_2a:  
-	tax                                     ; 4BA7 AA                       .
-	lda     LB14A,x                         ; 4BA8 BD 4A B1                 .J.
-	cmp     #$80                            ; 4BAB C9 80                    ..
-	bcs     L4BB7                           ; 4BAD B0 08                    ..
-	lda     #$01                            ; 4BAF A9 01                    ..
-	sta     $B800,x                         ; 4BB1 9D 00 B8                 ...
-	sta     L4764                           ; 4BB4 8D 64 47                 .dG
-L4BB7:  rts                                     ; 4BB7 60                       `
-
+	.include "cmd-2a.asm"
 	.include "sub-4bb8.asm"
 	.include "sub-4bc9.asm"
 	.include "sub-4bf2.asm"
