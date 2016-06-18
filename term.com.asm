c3722eea497443f9b74fb64131f96ac1e68ad0c8
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-18 15:01:57 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -1521,40 +1521,7 @@ sub_4B39:
 L4B44:  sta     $A0                             ; 4B44 85 A0                    ..
 	rts                                     ; 4B46 60                       `
 
-; ----------------------------------------------------------------------------
-sub_4B47:
-	stxa	off_AE
-	ldy     #$00                            ; 4B4B A0 00                    ..
-	lda     ($AE),y                         ; 4B4D B1 AE                    ..
-	cmp     #$24                            ; 4B4F C9 24                    .$
-	bne     L4B62                           ; 4B51 D0 0F                    ..
-	iny                                     ; 4B53 C8                       .
-	lda     ($AE),y                         ; 4B54 B1 AE                    ..
-	sec                                     ; 4B56 38                       8
-	sbc     #$30                            ; 4B57 E9 30                    .0
-	and     #$3F                            ; 4B59 29 3F                    )?
-	tax                                     ; 4B5B AA                       .
-	lda     LB118,x                         ; 4B5C BD 18 B1                 ...
-	sta     $A0                             ; 4B5F 85 A0                    ..
-	rts                                     ; 4B61 60                       `
-
-; ----------------------------------------------------------------------------
-L4B62:  jsr     sub_4B39
-	lda     $A0                             ; 4B65 A5 A0                    ..
-	asl     a                               ; 4B67 0A                       .
-	asl     a                               ; 4B68 0A                       .
-	asl     a                               ; 4B69 0A                       .
-	asl     a                               ; 4B6A 0A                       .
-	sta     $A1                             ; 4B6B 85 A1                    ..
-	iny                                     ; 4B6D C8                       .
-	lda     ($AE),y                         ; 4B6E B1 AE                    ..
-	jsr     sub_4B39
-	lda     $A0                             ; 4B73 A5 A0                    ..
-	clc                                     ; 4B75 18                       .
-	adc     $A1                             ; 4B76 65 A1                    e.
-	sta     $A0                             ; 4B78 85 A0                    ..
-	rts                                     ; 4B7A 60                       `
-
+	.include "sub-4b47.asm"
 	.include "sub-4b7b.asm"
 	.include "sub-4b97.asm"
 	.include "cmd-2a.asm"
