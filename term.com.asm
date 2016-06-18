1d2148f63a4a381bab5eff8df16f7af784f23ea1
Jason Duerstock <jason.duerstock@gmail.com>
2016-06-18 07:55:56 -0300

    tick

--- term.com.asm
+++ term.com.asm
@@ -1814,35 +1814,7 @@ sub_4E4A:
 	rdmv	$A0, L4E44
 	rts                                     ; 4EAA 60                       `
 
-; ----------------------------------------------------------------------------
-L4EAB:	.byte	$49,$46
-L4EAD:	.byte	$20,$67
-L4EAF:	.byte	$74,$28
-
-sub_4EB1:  
-	stack_prolog L4EAB, $05
-	add16m	off_AE, L4EAB, L4EAF
-	sub16i	L4EAB, off_AE, $0001
-	add16m	off_AE, L4EAD, L4EAF
-	sub16i	L4EAD, off_AE, $0001
-L4EFA:  lda     #$00                            ; 4EFA A9 00                    ..
-	cmp     L4EAF                           ; 4EFC CD AF 4E                 ..N
-	lda     #$00                            ; 4EFF A9 00                    ..
-	sbc     L4EAF+1
-	lbcs	L4F59
-	sub16i	L4EAF, L4EAF, $0001
-	dmv	off_AE, L4EAB
-	dmv	off_AC, L4EAD
-	ldy     #$00
-	lda     (off_AC),y
-	sta     (off_AE),y
-	sub16i	L4EAB, L4EAB, $0001
-	sub16i	L4EAD, L4EAD, $0001
-	jmp     L4EFA                           ; 4F56 4C FA 4E                 L.N
-
-; ----------------------------------------------------------------------------
-L4F59:  rts                                     ; 4F59 60                       `
-
+	.include "sub-4eb1.asm"
 	.include "cmd-07.asm"
 	.include "sub-4f6d.asm"
 	.include "cmd-25.asm"
