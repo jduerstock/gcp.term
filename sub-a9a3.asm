e63743e26c1a6d841c5fff138087e15bdfa7f8a0
Jason Duerstock <jason.duerstock@gmail.com>
2019-09-27 12:11:59 -0400

    tidy

--- sub-a9a3.asm
+++ sub-a9a3.asm
@@ -2,19 +2,13 @@
 ; ----------------------------------------------------------------------------
 sub_A9A3:
 	prolog
-	lda     $D8                             ; A9A6 A5 D8                    ..
-	eor     #$01                            ; A9A8 49 01                    I.
-	lbne	LA9B8
+	ifm8eqi	$D8, $01, LA9B8
 LA9AF:	proc8i	cmd_2a, $12
 	yldi	$D8, $00
-LA9B8:  lda     $D9                             ; A9B8 A5 D9                    ..
-	eor     #$01                            ; A9BA 49 01                    I.
-	lbne	LA9CA
+LA9B8:	ifm8eqi	$D9, $01, LA9CA
 	proc8i	cmd_2a, $13
 	yldi	$D9, $00
-LA9CA:  lda     $DA                             ; A9CA A5 DA                    ..
-	eor     #$01                            ; A9CC 49 01                    I.
-	lbne	LA9DC
+LA9CA:	ifm8eqi	$DA, $01, LA9DC
 	proc8i	cmd_2a, $14
 	yldi	$DA, $00
 LA9DC:  rts                                     ; A9DC 60                       `
